3d547f6b86f9da6247b3cf22948e48a7112f4276
Gregor Grigorjan <gregor@grigorjan.net>
2023-08-13 19:27:19 +0300

    tweak: footer font size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import SocialIcon from '@/components/social-icons'
 export default function Footer() {
   return (
     <footer>
-      <div className="mb-4 flex flex-col items-center text-gray-500 dark:text-gray-400">
+      <div className="mb-4 flex flex-col text-sm items-center text-gray-500 dark:text-gray-400">
         {`${siteMetadata.author} • Have a great day!`}
       </div>
     </footer>
