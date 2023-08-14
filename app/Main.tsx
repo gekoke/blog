54eb519f78e756c0a69b1e666dac46148a0c80c6
Gregor Grigorjan <gregor@grigorjan.net>
2023-08-14 04:25:34 +0300

    tweak: header sizes

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,7 +11,7 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100">
             Recent Posts
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
@@ -33,9 +33,9 @@ export default function Home({ posts }) {
                       </dd>
                     </dl>
                     <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
+                      <div className="space-y-2">
                         <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                          <h2 className="text-xl font-bold leading-8 tracking-tight">
                             <Link
                               href={`/blog/${slug}`}
                               className="text-gray-900 dark:text-gray-100"
