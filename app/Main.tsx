10e23805d077e1bcdaa657a07078c0cab5ec41d4
Gregor Grigorjan <gregor@grigorjan.net>
2023-08-14 04:25:41 +0300

    tweak: remove "Read more" in blog listings

--- app/Main.tsx
+++ app/Main.tsx
@@ -53,15 +53,6 @@ export default function Home({ posts }) {
                           {summary}
                         </div>
                       </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
                     </div>
                   </div>
                 </article>
